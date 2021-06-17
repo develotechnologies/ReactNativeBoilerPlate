3d9215daefbfd9051299338ed8080e6ea5b2bf55
Hammad Raza <hammad.raza@blssol.com>
2021-06-17 12:46:07 +0500

    new styles
    shoib bahi please add this

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,9 +1,9 @@
-import React, {Fragment} from 'react';
-import {View, Text, StatusBar, SafeAreaView} from 'react-native';
+import React, { Fragment } from 'react';
+import { View, Text, StatusBar, SafeAreaView } from 'react-native';
 import styles from './styles';
-import Button from '../../components/Button/Button.component';
-import {useDispatch, useSelector} from 'react-redux';
-import {login} from '../../Redux/Actions/Auth';
+import Button from '../../components/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../Redux/Actions/Auth';
 import ScreenWrapper from '../../components/ScreenWrapper';
 export default function Dashboard(props) {
   const user = useSelector((state) => state.Auth.user);
@@ -14,7 +14,7 @@ export default function Dashboard(props) {
         <Text> Login</Text>
         <Button
           title="Login"
-          onPress={() => dispatch(login({userName: 'John Doe'}))}
+          onPress={() => dispatch(login({ userName: 'John Doe' }))}
         />
       </View>
     </ScreenWrapper>
