af520b4c582c5f16632b62dca8bfaceeb232b905
Hammad Raza <hammad.raza@blssol.com>
2021-06-17 13:06:05 +0500

    Text Lock

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Text } from 'react-native';
 import App from './src';
-import {name as appName} from './app.json';
-
+import { name as appName } from './app.json';
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
 AppRegistry.registerComponent(appName, () => App);
