c4fac891f5b00be4b5e2c0cf349207f4826e70fa
RanaNasir018 <77691146+RanaNasir018@users.noreply.github.com>
2021-07-16 17:24:29 +0500

    NetInfo

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@ import Routes from './Routes/index';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import store from './Redux/index';
-import FlashMessage from 'react-native-flash-message';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
 import {LogBox} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+import {useEffect, useRef} from 'react';
 LogBox.ignoreAllLogs(true);
 export default function App() {
+  const networkConnected = useRef(true);
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      if (networkConnected != state.isConnected)
+        showMessage({
+          message: state.isConnected ? 'Back Online' : 'You are offline',
+          type: state.isConnected ? 'success' : 'warning',
+        });
+      networkConnected.current = state.isConnected;
+    });
+    return unsubscribe;
+  }, []);
   return (
     <Provider store={store}>
       <Routes />
-      <FlashMessage position="bottom" icon = "auto"/>
+      <FlashMessage position="bottom" icon="auto" />
     </Provider>
   );
 }
