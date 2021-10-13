abfcc83aea8338d6cdddbf341210582600c9e0ee
Hasham24 <31188910+Hasham24@users.noreply.github.com>
2021-10-13 12:32:52 +0500

    APi Manage Intercept

--- src/backend/ApiManager.js
+++ src/backend/ApiManager.js
@@ -1,69 +1,25 @@
 import axios from 'axios';
 import { BaseUrl } from './Config';
-axios.defaults.baseURL = BaseUrl;
+// axios.defaults.baseURL = BaseUrl;
 axios.interceptors.response.use(function (response) {
-    console.log(response)
-    return response;
+    return { ok: true, data: response?.data };
 }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+    return { ok: false, error: error?.response?.data }
 });
 export const ApiManager = {
     getData: async (endpoint, params = {}) => {
-        let res;
-        try {
-            res = await axios.get(endpoint, { params });
-            if (res?.status === 200) {
-                return { ok: true, data: res.data };
-            } else return { ok: false, data: [] };
-        } catch (error) {
-            return { ok: false, error: error.response.data };
-        }
+        return axios.get(endpoint, { params });
     },
     postData: async (endpoint, data, params = {}) => {
-        let res;
-        try {
-            res = fetch(BaseUrl + endpoint, JSON.stringify(data));
-            res = await axios.post(endpoint, data, { params });
-            if (res?.status === 200) {
-                return { ok: true, data: res.data };
-            } else return { ok: false, data: null };
-        } catch (error) {
-            return { ok: false, error: error.response.data };
-        }
+        return axios.post(endpoint, data, { params });
     },
     putData: async (endpoint, data, params = {}) => {
-        let res;
-        try {
-            res = await axios.put(endpoint, data, { params });
-            if (res?.status === 200) {
-                return { ok: true, data: res.data };
-            } else return { ok: false, data: [] };
-        } catch (error) {
-            return { ok: false, error: error.response.data };
-        }
+        return axios.put(endpoint, data, { params });
     },
     patchData: async (endpoint, data, params = {}) => {
-        let res;
-        try {
-            res = await axios.patch(endpoint, data, { params });
-            if (res?.status === 200) {
-                return { ok: true, data: res.data };
-            } else return { ok: false, data: [] };
-        } catch (error) {
-            return { ok: false, error: error.response.data };
-        }
+        return axios.patch(endpoint, data, { params });
     },
     deleteData: async (endpoint, params) => {
-        let res;
-        try {
-            res = await axios.delete(endpoint, { params });
-            if (res?.status === 200) {
-                return { ok: true, data: res.data };
-            } else return { ok: false, data: [] };
-        } catch (error) {
-            return { ok: false, error: error.response.data };
-        }
+        return axios.delete(endpoint, { params });
     },
 };
--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -8,16 +8,22 @@ import BGImage from '../../assets/images/bg.jpg';
 import { ScreenWrapper } from '../../components/ScreenWrapper';
 import AppColors from '../../utills/AppColors';
 import { showMessage } from 'react-native-flash-message';
+import { ApiManager } from '../../backend/ApiManager'
 export default function Dashboard(props) {
   const user = useSelector((state) => state.Auth.user);
   const dispatch = useDispatch();
-  const logoutMethod = () => {
-    showMessage({
-      message: 'Logged Out',
-      description: 'Succfully logged out',
-      type: 'danger',
-    });
-    dispatch(logout());
+  const logoutMethod = async () => {
+    const response = await ApiManager.getData(`https://reactnative.dev/movies.json`)
+    if (response?.ok)
+      console.log(response.data);
+    else
+      console.log("Error", response.error)
+    // showMessage({
+    //   message: 'Logged Out',
+    //   description: 'Succfully logged out',
+    //   type: 'danger',
+    // });
+    // dispatch(logout());
   };
   return (
     <ScreenWrapper backgroundImage={BGImage} transclucent statusBarColor={AppColors.transparent} barStyle="light-content">
